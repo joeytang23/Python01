46d8083f5a603376ac3d75ce021dbaec6a7ec6b6
joeytang23 <3251734073@qq.com>
2026-06-28 11:22:01 +0800

    streamlit入门

--- 第三章/03.ai_partner_1.py
+++ 第三章/03.ai_partner_1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 from openai import OpenAI
+
 #设置页面配置
 st.set_page_config(
     page_title="AI智能伴侣",
@@ -21,8 +22,18 @@ st.logo("resources/logo.png")
 
 system_prompt = "你是一名非常可爱的AI助理，你的名字叫小甜甜，请你使用温柔可爱的语气回答用户的问题"
 
+#初始化聊天信息
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
 #创建客户端对象
 
+#展示聊天信息
+for message in st.session_state.messages:
+    st.chat_message(message["role"]).write(message["content"])
+    #if message["role"] == "user":
+        #st.chat_message("user").write(message["content"])
+    #else:
+        #st.chat_message("assistant").write(message["content"])
 client = OpenAI(api_key=os.environ.get('DEEPSEEK_API_KEY'),base_url="https://api.deepseek.com")
 #输入框
 
@@ -30,6 +41,8 @@ prompt = st.chat_input("请输入您的问题")
 if prompt:#字符串自动转换布尔值
     st.chat_message("user").write(prompt)
     print("------------->调用AI大模型,提示词",prompt)
+    # 添加用户消息到会话状态
+    st.session_state.messages.append({"role": "user", "content": prompt})
     response = client.chat.completions.create(
         model="deepseek-v4-pro",
         messages=[
@@ -42,5 +55,7 @@ if prompt:#字符串自动转换布尔值
     )
     print("<------------------ 大模型返回的结果", response.choices[0].message.content)
     st.chat_message("assistant").write(response.choices[0].message.content)
+    # 添加AI助手消息到会话状态
+    st.session_state.messages.append({"role": "assistant", "content": response.choices[0].message.content})
 
 
