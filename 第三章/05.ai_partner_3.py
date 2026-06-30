b3d340c503a93572dedb00d883ab4f923d2ba316
joeytang23 <3251734073@qq.com>
2026-06-30 15:51:05 +0800

    会话记忆和侧边栏

--- 第三章/05.ai_partner_3.py
+++ 第三章/05.ai_partner_3.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import os
 from openai import OpenAI
-#加入了会话记忆
-
+#加了侧边栏
 #设置页面配置
 st.set_page_config(
     page_title="AI智能伴侣",
@@ -18,20 +17,20 @@ st.title("AI智能伴侣")
 
 st.logo("resources/logo.png")
 
-# #侧边栏
-# with st.sidebar:
-#      st.subheader("伴侣信息")
-#      nick_name =  st.text_input("昵称",value="小甜甜",placeholder="请输入伴侣昵称")
-#      if nick_name:
-#          st.session_state.nick_name = nick_name
-#      character = st.text_area("伴侣性格",value="温柔细腻的台湾姑娘",placeholder="请输入伴侣性格")
-#      if character:
-#          st.session_state.character = character
+#侧边栏
+with st.sidebar:
+     st.subheader("伴侣信息")
+     nick_name =  st.text_input("昵称",value="小甜甜",placeholder="请输入伴侣昵称")
+     if nick_name:
+         st.session_state.nick_name = nick_name
+     character = st.text_area("伴侣性格",value="温柔细腻的台湾姑娘",placeholder="请输入伴侣性格")
+     if character:
+         st.session_state.character = character
 
 #系统提示词
 
 system_prompt = """
-你叫小甜甜，现在是用户的真实伴侣，请完全代入伴侣角色。：
+你叫%s，现在是用户的真实伴侣，请完全代入伴侣角色。：
 规则：
   1. 每次只回1条消息
   2. 禁止任何场景或状态描述性文字
@@ -41,17 +40,17 @@ system_prompt = """
   6. 用符合伴侣性格的方式对话
   7. 回复的内容,要充分体现伴侣的性格特征
 伴侣性格：
-  - 温柔细腻的台湾姑娘
+  - %s
 你必须严格遵守上述规则来回复用户。
 """
 
 #初始化聊天信息
 if 'messages' not in st.session_state:
     st.session_state['messages'] = []
-# if 'nick_name' not in st.session_state:
-#     st.session_state['nick_name'] = '小甜甜'
-# if 'character' not in st.session_state:
-#     st.session_state['character'] = '温柔细腻的台湾姑娘'
+if 'nick_name' not in st.session_state:
+    st.session_state['nick_name'] = '小甜甜'
+if 'character' not in st.session_state:
+    st.session_state['character'] = '温柔细腻的台湾姑娘'
 
 #展示聊天信息
 for message in st.session_state.messages:
@@ -72,7 +71,7 @@ if prompt:#字符串自动转换布尔值
     response = client.chat.completions.create(
         model="deepseek-v4-pro",
         messages=[
-            {"role": "system","content": system_prompt },
+            {"role": "system","content": system_prompt % (st.session_state.nick_name, st.session_state.character)},
             *st.session_state.messages #会话记忆
         ],
         stream=True,
